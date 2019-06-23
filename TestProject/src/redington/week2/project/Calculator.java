17cc5723d53795a8e3edfbd72286784d306e6442
SaranyaRukmani <50944322+SaranyaRukmani@users.noreply.github.com>
2019-06-23 21:09:36 +0530

    calculator class

--- TestProject/src/redington/week2/project/Calculator.java
+++ TestProject/src/redington/week2/project/Calculator.java
@@ -4,7 +4,35 @@ public class Calculator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+       Calculator calc=new Calculator();
+       calc.add();
+       calc.sub();
+       calc.div();
+       calc.mul();
+	}
+	void add()
+	{
+		int a=10,b=80,total;
+		total=a+b;
+		System.out.println("Addition is "+total);
+	}
+	void sub()
+	{
+		int a=80,b=10,total;
+		total=a-b;
+		System.out.println("Subtraction is "+total);
+	}
+	void div()
+	{
+		int a=80,b=10,total;
+		total=a/b;
+		System.out.println("Division is "+total);
 	}
 
+	void mul()
+	{
+		int a=80,b=10,total;
+		total=a*b;
+		System.out.println("Multiplication is "+total);
+	}
 }
